c2a439dfd54d95bf2ceffc04d1e6bd4e3dd92f2c
Kimberly_Glick <kimberlyjglick@gmail.com>
2020-07-22 21:28:05 -0400

    user can now select list of activities to pick

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 //lists the user can pick from to generate activities from
-const yourSavedActivities = ["Study CSS", "Study JavaScript", "Work on your own coding project", "Practice guitar - technical skills", "Practice guitar - learn a cover", "Practice guitar - practice your own songs", "Revise or edit a mostly or totally finished song of your own", "Work on an in-process song", "Come up with 10 song content ideas", "Take the 5th song down on the 14th playlist down on spotify. Use that song structure to write a song", "Work in the garden", "Clean the house", "Upload photos for Dad", "Scan photos", "Date and organize old photos", "Go on a walk", "Go for a run", "Do pushups", "Read", "Watch TV", "Learn about racism", "Take action against racism", "Draw"];
-const productiveActivities = ["Coding", "Practice guitar", "Write Songs", "Sort through old papers"];
-const funActivities = ["Nap", "Read", "Watch TV", "Draw", "Go for a walk", "Call a Friend"];
-const creativeActivities = ["Draw", "Write a Song", "Write a Story"];
-const randomActivities = ["Draw Mike Wasouski", "Go on a mile plus bike ride",  "Stare into your own eyes in the mirror", "Nap", "Run around your house", "Make a paper airplane", "Make a pair of shorts from an old sheet", "Make a home movie using a plot from Plot Generator.", "Bake homemade pizza", "Dress up in the fanciest clothes you can find", "Host a black-tie video chat", "Host a black-tie garden walk", "Write a coronavirus parody of your favourite song", "Build a toothpick bridge"]
-const enterActivities = [];
+const allTheLists = {
+  //yourSavedActivities
+  activities1: ["Study CSS", "Study JavaScript", "Work on your own coding project", "Practice guitar - technical skills", "Practice guitar - learn a cover", "Practice guitar - practice your own songs", "Revise or edit a mostly or totally finished song of your own", "Work on an in-process song", "Come up with 10 song content ideas", "Take the 5th song down on the 14th playlist down on spotify. Use that song structure to write a song", "Work in the garden", "Clean the house", "Upload photos for Dad", "Scan photos", "Date and organize old photos", "Go on a walk", "Go for a run", "Do pushups", "Read", "Watch TV", "Learn about racism", "Take action against racism", "Draw"],
+  //productiveActivities
+  activities2: ["Coding", "Practice guitar", "Write Songs", "Sort through old papers"],
+  //funActivities
+  activities3: ["Nap", "Read", "Watch TV", "Draw", "Go for a walk", "Call a Friend"],
+  //creativeActivities
+  actitivies4: ["Draw", "Write a Song", "Write a Story"],
+  //randomActivities
+  actitivies5: ["Draw Mike Wasouski", "Go on a mile plus bike ride",  "Stare into your own eyes in the mirror", "Nap", "Run around your house", "Make a paper airplane", "Make a pair of shorts from an old sheet", "Make a home movie using a plot from Plot Generator.", "Bake homemade pizza", "Dress up in the fanciest clothes you can find", "Host a black-tie video chat", "Host a black-tie garden walk", "Write a coronavirus parody of your favourite song", "Build a toothpick bridge"],
+  //enter your own activities
+  activies6: []
+}
 
 //binds html elements to variables
 //displays
@@ -14,22 +22,26 @@ const display2 = document.getElementById('display-2');
 const display3 = document.getElementById('display-3');
 const display4 = document.getElementById('display-4');
 const display5 = document.getElementById('display-5');
+const display6 = document.getElementById('display-6');
 //buttons
 const userInputButton = document.getElementById("user-input-button");
 //user inputs
 let userName = "";
 let userTotalTime = 0;
 let userMinimumTime = 0;
-
+let userActivityListChoice = [];
 //functions to use in the main function (modules?)
 let returnedToDoList = [];
 let numberOfActivities = 0;
 let timeBlocks = [];
+let testSave = "";
 
 const getUserInputs = () => {
   userName = document.getElementById('your-name').value;
   userTotalTime = parseInt(document.getElementById('minutes-to-fill').value, 10);
   userMinimumTime = parseInt(document.getElementById('minimum-time-block').value, 10);
+  testSave = document.getElementById('activity-option-set').value;
+  userActivityListChoice = allTheLists[testSave];
 }
 
 const getNumberOfActivities = () => {
@@ -63,7 +75,7 @@ userInputButton.addEventListener('click', () => {
   getUserInputs();
   getNumberOfActivities();
   returnedToDoList = [];
-  curateActivityList(yourSavedActivities, numberOfActivities);
+  curateActivityList(userActivityListChoice, numberOfActivities);
   getTimeBlocks(userTotalTime, numberOfActivities);
 
   alert(userName + " - please actually do what we tell you to. Otherwise what's the point?");
@@ -74,6 +86,7 @@ userInputButton.addEventListener('click', () => {
   display3.innerHTML = "number of activities: " + (numberOfActivities);
   display4.innerHTML = 'Activity List:<br>' + (returnedToDoList.join("<br>"));
   display5.innerHTML = `Time blocks ${timeBlocks}`;
+  display6.innerHTML = `You chose ${userActivityListChoice} activity list`;
 })
 
 
